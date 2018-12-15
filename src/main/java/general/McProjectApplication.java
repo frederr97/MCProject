db3ee9a47ca8f405235241e344efadf52b6a1051
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2018-12-15 17:12:09 -0200

    Initial Commit

---
+++ src/main/java/general/McProjectApplication.java
@@ -0,0 +1,14 @@
+package general;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McProjectApplication.class, args);
+	}
+
+}
+
