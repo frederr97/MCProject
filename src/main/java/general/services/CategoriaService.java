c546ead128cfa943ad93bcad743885d371d2eb10
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2018-12-19 16:22:25 -0200

    Implementation of CRUD

--- src/main/java/general/services/CategoriaService.java
+++ src/main/java/general/services/CategoriaService.java
@@ -1,13 +1,14 @@
 package general.services;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import general.domain.Categoria;
 import general.repositories.CategoriaRepository;
+import general.services.exceptions.DataIntegrityException;
 import general.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +30,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados!");
+		}
 	}
 }
