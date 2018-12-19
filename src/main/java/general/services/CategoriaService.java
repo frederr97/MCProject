5d5e9637bc08c4b7e7814202ef4ca289d99db27a
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2018-12-19 17:23:07 -0200

    DTO implementation

--- src/main/java/general/services/CategoriaService.java
+++ src/main/java/general/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package general.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados!");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
