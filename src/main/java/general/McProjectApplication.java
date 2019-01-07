78e02e5cb2203e26effcfd6ef39bd74dca221c34
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-07 21:23:17 -0200

    Amazon S3 sincronization

--- src/main/java/general/McProjectApplication.java
+++ src/main/java/general/McProjectApplication.java
@@ -1,17 +1,11 @@
 package general;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import general.services.S3Service;
-
 @SpringBootApplication
 public class McProjectApplication implements CommandLineRunner{
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(McProjectApplication.class, args);
@@ -19,7 +13,6 @@ public class McProjectApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("src/main/resources/images/woman.jpg");
 	}
 }
 
